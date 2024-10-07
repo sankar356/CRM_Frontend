7e8cf3d30428d833c320e3a9b95757a4076d2574
siva <sankarmuthusiva356@gmail.com>
2024-10-07 18:54:04 +0530

    dashbord and theme

--- src/layout/header/header.component.ts
+++ src/layout/header/header.component.ts
@@ -1,16 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { LayoutComponent } from '../layout/layout.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,LayoutComponent],
+  imports: [CommonModule,LayoutComponent,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  navList = [""]
-  naves = new LayoutComponent;
-  nave = this.naves.nave
+export class HeaderComponent implements OnInit{
+  isDark : boolean = false;
+
+  constructor(){}
+  ngOnInit(): void {
+    this.isDark =localStorage.getItem('theme') === 'night';
+    this.applyTheme();
+  }
+
+  toggleThem(): void{
+    this.isDark = !this.isDark;
+    localStorage.setItem('theme', this.isDark ? 'night' : 'nord');
+    this.applyTheme();
+  }
+  applyTheme(): void {
+    // Apply the theme using DaisyUI's theme switching
+    document.documentElement.setAttribute('data-theme', this.isDark ?  'night' : 'nord');
+  }
 }
